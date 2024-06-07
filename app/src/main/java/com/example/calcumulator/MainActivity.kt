894e1b7cb0c6337e18985e8e13de0c8d9000c509
Aryan Chand <aryanchand753@gmail.com>
2024-06-06 21:52:41 -0400

    added basic logic

--- app/src/main/java/com/example/calcumulator/MainActivity.kt
+++ app/src/main/java/com/example/calcumulator/MainActivity.kt
@@ -12,36 +12,55 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.calcumulator.ui.theme.CalcumulatorTheme
+import android.app.AppCompatActivity
+import android.view.View // Needed for click listener interfaces (OnClickListener)
+import android.widget.Button // Needed for Button class
+import android.widget.TextView // Needed for TextView class
 
 class MainActivity : ComponentActivity() {
+
+    private var firstNumber: Double? = null
+    private var secondNumber: Double? = null
+    private var operation: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            CalcumulatorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Aryan",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        // Button Operation Listeners
+        findViewById<Button>(R.id.ac).setOnClickListener { clearAll() }
+        findViewById<Button>(R.id.slh).setOnClickListener { switchSign() }
+        findViewById<Button>(R.id.mod).setOnClickListener { operation = "%" }
+        findViewById<Button>(R.id.div).setOnClickListener { operation = "/" }
+        findViewById<Button>(R.id.mult).setOnClickListener { operation = "*"}
+        findViewById<Button>(R.id.minus).setOnClickListener { operation = "-" }
+        findViewById<Button>(R.id.plus).setOnClickListener { operation = "+" }
+        findViewById<Button>(R.id.dot).setOnClickListener { appendDecimal() }
+        findViewById<Button>(R.id.equal).setOnClickListener { calculateResult() }
+
+        // Number Button listeners
+        val numberButtons = arrayOf(
+            R.id.zero, R.id.one, R.id.two, R.id.three, R.id.four, R.id.five, R.id.six, R.id.seven, R.id.eight, R.id.nine
+        )
+        for (id in numberButtons) {
+            val button = findViewById<Button>(id)
+            button.setOnClickListener { appendNumber(it.text.toString()) }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CalcumulatorTheme {
-        Greeting("Android")
+    private fun clearAll() {
+        firstNumber = null
+        secondNumber = null
+        operation = null
+        findViewById<TextView>(R.id.result).text = "0.0"
     }
+
+    private fun switchSign() {  }
+
+    private fun appendNumber(number: String) {  }
+
+    private fun appendDecimal() {  }
+
+    private fun calculateResult() {  }
+
+
 }
